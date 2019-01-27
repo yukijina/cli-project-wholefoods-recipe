eae456319952c88aa43cd06781d882c6a231a0c9
yukijina <yukijina@gmail.com>
2019-01-27 14:43:04 -0800

    add initialize Recipe.new

--- lib/wholefoods_recipe/scraper.rb
+++ lib/wholefoods_recipe/scraper.rb
@@ -6,12 +6,11 @@ class Scraper
     doc.css(".recipe-item").each do |collection|
       title = collection.css("h3").text
       url = "https://www.wholefoodsmarket.com" + collection.css("h3 a").attr("href")
-      recipe = Category.new(title, url)
+      category = Category.new(title, url)
     end
   end
 
   def self.scraper_recipe
-    binding.pry
     urls = Category.all.map {|category| category.url}
 
     urls.each do |url|
@@ -22,12 +21,14 @@ class Scraper
         doc.css(".he-recipes").each do |data|
           name = data.css("p a").text
           url = "https://www.wholefoodsmarket.com" + data.css("p a").attr("href")
+          recipe = Recipe.new(title, name, url)
         end
       elsif doc.css(".views-row")
         title = doc.css(".field-content h2").text
         doc.css(".views-field-title").each do  |data|
           name = data.css("a").text
           url = "https://www.wholefoodsmarket.com" + data.css("a").attr("href")
+          recipe = Recipe.new(title, name, url)
         end
       end
 
