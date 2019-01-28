4e7dec3def543eb1f91be3e98085ac10bcffa818
yukijina <yukijina@gmail.com>
2019-01-27 16:36:08 -0800

    updaed cli, added Recipe class method

--- lib/wholefoods_recipe/cli.rb
+++ lib/wholefoods_recipe/cli.rb
@@ -2,10 +2,7 @@ class CLI
 
  def call
    puts "Welcome to Wholfoods Recipe CLI!!!"
-   Scraper.scraper_category
-   Scraper.scraper_recipe
    list_category
-
  end
 
  def list_category
@@ -14,6 +11,7 @@ class CLI
    input = gets.strip
    if input == "list"
      Scraper.scraper_category
+     Scraper.scraper_recipe
      Category.all.each.with_index(1) do |list, index|
        puts "#{index}. #{list.title}"
      end
@@ -27,9 +25,18 @@ class CLI
 
  def list
    puts "Please type the number you are interested in or type exit: "
+
    input = gets.strip
    if input.to_i <= Category.all.size && input.to_i > 0
-     puts "More inforamtion for #{input.to_i}"
+     index = input.to_i-1
+     category= Category.all.map{|list| list.title}
+     puts "You selected #{input}. #{category[index]}!"
+     Recipe.all.each do |list|
+       if list.title == category[index].strip
+         list_details
+       end
+     end
+
    elsif
      input == "exit"
      goodbye
@@ -37,9 +44,18 @@ class CLI
      input.to_i == 0
      list
    end
-   #1.name of the receipes
-   #2.receipe url
-   #3.need to get user input
+ end
+
+ def list_details
+   binding.pry
+   puts "Please type recipe name you like: "
+   input = gets.strip
+   puts "You selected #{input}"
+   Recipe.all.each do |list|
+     if list.name.downcase == input.downcase
+       system("open #{a=list.url}")
+     end
+   end
  end
 
  def goodbye
