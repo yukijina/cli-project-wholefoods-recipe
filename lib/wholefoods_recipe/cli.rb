9d638ae1e258669989e8edd2b0019784102fd1bd
yukijina <yukijina@gmail.com>
2019-01-29 06:52:53 -0800

    refactor for better flow

--- lib/wholefoods_recipe/cli.rb
+++ lib/wholefoods_recipe/cli.rb
@@ -1,25 +1,29 @@
 class WholefoodsRecipe::CLI
 
  def call
-   puts "Welcome to Wholfoods Recipe CLI!!!"
+   puts "Welcome to Wholfoods Recipe Collection!!!"
    list_category
  end
 
  def list_category
    puts "Please type list to see recipe categories or type exit: "
-   input = nil
-   input = gets.strip
+   input = gets.strip.downcase
    if input == "list"
      WholefoodsRecipe::Scraper.scraper_category
-     WholefoodsRecipe::Category.all.each.with_index(1) do |list, index|
-       puts "#{index}. #{list.title}"
-     end
-     select_number
+     display
    elsif input == "exit"
      goodbye
    else
+     puts "Invalid."
      list_category
    end
+ end
+
+ def display
+   WholefoodsRecipe::Category.all.each.with_index(1) do |list, index|
+     puts "#{index}. #{list.title}"
+   end
+   select_number
  end
 
  def select_number
@@ -27,12 +31,11 @@ class WholefoodsRecipe::CLI
    input = gets.strip
    if input.to_i <= WholefoodsRecipe::Category.all.size && input.to_i > 0
      index = input.to_i-1
-     category_title = WholefoodsRecipe::Category.all.map{|category| category.title}
-     @user_select = category_title[index]
+     @user_select = WholefoodsRecipe::Category.all[index].title
      puts "You selected #{input}.#{@user_select}!"
      display_link
    elsif
-     input == "exit"
+     input.downcase == "exit"
      goodbye
    elsif
      input.to_i <= 0 || input.to_i > WholefoodsRecipe::Category.all.size
@@ -42,17 +45,27 @@ class WholefoodsRecipe::CLI
  end
 
  def display_link
-   puts "Would you like to open link to see more details? [y/n]"
-   input = gets.strip
-   if input.downcase == "yes" ||  input.downcase == "y"
+   puts "Would you like to open the link to see more details? [y/n]"
+   input = gets.strip.downcase
+   if input == "yes" || input == "y"
      puts "Here you go!!"
      WholefoodsRecipe::Category.all.each do |list|
        if @user_select == list.title
          system("open #{list.url}")
        end
      end
+   elsif input == "no" ||  input == "n"
+     puts "Would you like to select another recipe collection [y/n]?"
+     answer = gets.strip.downcase
+     if answer == "yes" || answer == "y"
+       puts "OK!"
+       display
+     else
+       goodbye
+     end
    else
-     goodbye
+     puts "Invalid."
+     display_link
    end
  end
 
