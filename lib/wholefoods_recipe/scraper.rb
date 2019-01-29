0c70e027d69268121f62c3cac864ad312890b20d
yukijina <yukijina@gmail.com>
2019-01-29 06:51:34 -0800

    miner fix

--- lib/wholefoods_recipe/scraper.rb
+++ lib/wholefoods_recipe/scraper.rb
@@ -6,7 +6,7 @@ class WholefoodsRecipe::Scraper
     doc.css(".recipe-item").each do |collection|
       title = collection.css("h3").text.strip
       url = "https://www.wholefoodsmarket.com" + collection.css("h3 a").attr("href")
-      category = WholefoodsRecipe::Category.new(title, url)
+      WholefoodsRecipe::Category.new(title, url)
     end
   end
 
