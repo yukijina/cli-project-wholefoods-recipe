8f9a1fe158894f480d0f714642a62ddd7244447b
yukijina <yukijina@gmail.com>
2019-01-29 13:16:59 -0800

    assigned attribute value in initiazlie, created all methods

---
+++ lib/wholefoods_recipe/recipe.rb
@@ -0,0 +1,16 @@
+class WholefoodsRecipe::Recipe
+  attr_accessor :recipe_name, :recipe_url
+  @@all = []
+
+  def initialize(recipe_name, recipe_url)
+    @recipe_name = recipe_name
+    @recipe_url = recipe_url
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+
+end
