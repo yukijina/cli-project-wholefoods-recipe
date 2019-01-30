d9684cd6ec2a934e95088874ddfab581ad730c9f
yukijina <yukijina@gmail.com>
2019-01-29 22:09:51 -0800

    scraped recipe name and url in scraper_recipes method

--- lib/wholefoods_recipe/scraper.rb
+++ lib/wholefoods_recipe/scraper.rb
@@ -13,13 +13,23 @@ class WholefoodsRecipe::Scraper
   def self.scraper_recipes(url)
     doc = Nokogiri::HTML(open(url))
 
-    doc.css(".he-recipes p a").each do |element|
-      binding.pry
-      recipe_name = element.text
-      recipe_url = element.attr("href")
-      WholefoodsRecipe::Recipe.new(recipe_name, recipe_url)
+    if doc.at_css(".he-recipes")
+      doc.css(".he-recipes p a").each do |element|
+        if !element.attr("href").include?("blog")
+          recipe_name = element.text.strip
+          recipe_url = "https://www.wholefoodsmarket.com" + element.attr("href")
+        end
+        WholefoodsRecipe::Recipe.new(recipe_name, recipe_url) unless recipe_url == nil
+      end
+    elsif doc.at_css(".he-recipes") == nil
+      doc.css("span.field-content a").each do |element|
+        if !element.attr("href").include?("blog")
+          recipe_name = element.text
+          recipe_url = "https://www.wholefoodsmarket.com" + element.attr("href")
+        end
+        WholefoodsRecipe::Recipe.new(recipe_name, recipe_url) unless recipe_url == nil
+      end
     end
-
   end
 
 end
