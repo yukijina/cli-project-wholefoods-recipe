efff9f2ce7e390118a7250623050ebdc031159b2
yukijina <yukijina@gmail.com>
2019-01-30 06:18:03 -0800

    initialized and assigned attribut valu from scraper recipe detailse

--- lib/wholefoods_recipe.rb
+++ lib/wholefoods_recipe.rb
@@ -6,6 +6,7 @@ require_relative "./wholefoods_recipe/version"
 require_relative "./wholefoods_recipe/cli"
 require_relative "./wholefoods_recipe/category"
 require_relative "./wholefoods_recipe/recipes"
+require_relative "./wholefoods_recipe/recipe"
 require_relative "./wholefoods_recipe/scraper"
 
 
