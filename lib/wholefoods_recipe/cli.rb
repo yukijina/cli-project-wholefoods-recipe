53233e9046631700f691806553b8256a8c7acac4
yukijina <yukijina@gmail.com>
2019-01-30 07:01:28 -0800

    updated display_recipe_details to display recipe

--- lib/wholefoods_recipe/cli.rb
+++ lib/wholefoods_recipe/cli.rb
@@ -10,7 +10,7 @@ class WholefoodsRecipe::CLI
    input = gets.strip.downcase
    if input == "list"
      WholefoodsRecipe::Scraper.scraper_category
-     display
+     display_category
    elsif input == "exit"
      goodbye
    else
@@ -19,7 +19,7 @@ class WholefoodsRecipe::CLI
    end
  end
 
- def display
+ def display_category
    WholefoodsRecipe::Category.all.each.with_index(1) do |list, index|
      puts "#{index}. #{list.title}"
    end
@@ -58,7 +58,7 @@ class WholefoodsRecipe::CLI
    if input.to_i <= WholefoodsRecipe::Recipes.all.size && input.to_i > 0
      index = input.to_i-1
      @user_select = WholefoodsRecipe::Recipes.all[index]
-     puts "Here is the details for #{input}.#{@user_select.recipe_name}!"
+     puts "Here is the details for #{input}.#{@user_select.recipe_name}!!"
      display_recipe_details
    elsif
      input.downcase == "exit"
@@ -72,6 +72,19 @@ class WholefoodsRecipe::CLI
 
  def display_recipe_details
    WholefoodsRecipe::Scraper.scraper_recipe_details(@user_select.recipe_url)
+   WholefoodsRecipe::Recipe.all.each do |recipe_details|
+     puts ""
+     puts "*** #{recipe_details.title} ***"
+     puts ""
+     puts "~ #{recipe_details.description} ~"
+     puts ""
+     puts "What you need: "
+     recipe_details.ingredients.each.with_index(1) do |ingredient, index|
+       puts "#{index}. #{ingredient}"
+     end
+   end
+   puts ""
+   puts "Enjoy cooking!!!"
  end
 
  def display_link
