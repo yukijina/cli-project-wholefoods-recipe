ce4b790a2669723c903054dbea02f136536814a1
yukijina <yukijina@gmail.com>
2019-02-14 07:42:24 -0800

    added set_addtional_attirbute method in order to combine recipes.rb

--- lib/wholefoods_recipe/recipe.rb
+++ lib/wholefoods_recipe/recipe.rb
@@ -1,9 +1,14 @@
 class WholefoodsRecipe::Recipe
-  attr_accessor :name, :description, :ingredients
+  attr_accessor :name, :url, :description, :ingredients
   @@all = []
 
-  def initialize(name, description, ingredients)
+  def initialize(name, url)
     @name = name
+    @url = url
+    @@all << self
+  end
+
+  def set_additonal_attribute(description, ingredients)
     @description = description
     @ingredients = ingredients
     @@all << self
