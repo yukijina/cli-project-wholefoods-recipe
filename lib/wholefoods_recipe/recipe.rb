5c64d61b2f351eca688e324f028fe9851188cbfb
yukijina <yukijina@gmail.com>
2019-02-15 06:58:04 -0800

    deleted comment

--- lib/wholefoods_recipe/recipe.rb
+++ lib/wholefoods_recipe/recipe.rb
@@ -9,16 +9,8 @@ class WholefoodsRecipe::Recipe
   end
 
   def set_additonal_attribute(description, ingredients)
-
-     @description = description
-     @ingredients = ingredients
-    # @@all << self
-# binding.pry
-#     @@all.each do |recipe|
-#       recipe.description = @description
-#       recipe.ingredients = @ingredients
-    #end
-
+    @description = description
+    @ingredients = ingredients
   end
 
   def self.all
