1aabc9e818dd53af931c86393646d9b238b36667
yukijina <yukijina@gmail.com>
2019-02-15 06:55:36 -0800

    deleted comment

--- lib/wholefoods_recipe.rb
+++ lib/wholefoods_recipe.rb
@@ -5,7 +5,6 @@ require 'pry'
 require_relative "./wholefoods_recipe/version"
 require_relative "./wholefoods_recipe/cli"
 require_relative "./wholefoods_recipe/category"
-require_relative "./wholefoods_recipe/recipes"
 require_relative "./wholefoods_recipe/recipe"
 require_relative "./wholefoods_recipe/scraper"
 
